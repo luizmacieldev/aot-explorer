281039a4ead88c72b14e51e51f670dc6192d77a6
luizlgam <lgamjoeperry@gmail.com>
2026-04-11 19:10:09 -0300

    Update on titan card

--- frontend/aot/app/(aot)/titans/page.tsx
+++ frontend/aot/app/(aot)/titans/page.tsx
@@ -17,7 +17,7 @@ export default function TitansPage() {
   return (
     <div className="p-8 max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
     {titans.map((titan) => (
-      <Card key={titan.id} item={titan} href="/titans" />
+      <Card key={titan.id} item={titan} href="/titans" type="titan" />
     ))}
     </div>
   );
